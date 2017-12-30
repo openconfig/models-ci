d7bce778f95c545b542f0cc67933f831e468652e
Anees Shaikh <aashaikh@google.com>
2017-12-30 00:23:07 -0800

    Add options and checks to doc gen master script.
      * (M) bin/gen_docs_branch.sh
        - Added command line options to override defaults
        - Added sanity checks for input parameters

--- bin/gen_docs_branch.sh
+++ bin/gen_docs_branch.sh
@@ -17,21 +17,76 @@
 # Wrapper script to generate html documentation for a specified branch
 #
 
+# set some defaults for use with the webhook
 OC_STAGE_DIR=/home/ghci/oc-tools/oc-stage
 OC_PYANG_PLUGINS=/home/ghci/oc-pyang/openconfig_pyang/plugins
 DOC_OUTPUT=/opt/nginx/nginx/html/branches
 
 
-if [ -z ${GITHUB_ACCESS_TOKEN} ]
-then
-  echo "GITHUB_ACCESS_TOKEN not set" >&2
+show_usage () {
+  # print usage statement
+  echo "$0 -h -s <oc-stage dir> -p <docs plugin dir> -o <output dir>" >&2
   exit 1
-fi
+}
 
-if [ -z ${PUSH_BRANCH} ]
-then
-  echo "PUSH_BRANCH variable not set" >&2
-  exit 1
-fi
+check_args () {
+  # sanity check pre-requisites
+
+  if [ -z ${GITHUB_ACCESS_TOKEN} ]
+  then
+    echo "GITHUB_ACCESS_TOKEN not set" >&2
+    exit 1
+  fi
+
+  if [ -z ${PUSH_BRANCH} ]
+  then
+    echo "PUSH_BRANCH variable not set" >&2
+    exit 1
+  fi
+
+  if [ ! -d ${OC_STAGE_DIR} ]
+  then
+    echo "oc-tools/oc-stage directory $OC_STAGE_DIR does not exist" >&2
+    exit 1
+  fi
+
+  if [ ! -d ${OC_PYANG_PLUGINS} ]
+  then
+    echo "oc-pyang plugins directory $OC_PYANG_PLUGINS does not exist" >&2
+    exit 1
+  fi
+
+  if [ ! -d ${DOC_OUTPUT} ]
+  then
+    echo "output directory $DOC_OUTPUT does not exist" >&2
+    exit 1
+  fi
+
+}
+
+
+# options are for overriding defaults, primarily
+# while testing
+while getopts "hs:o:p:" opt; do
+  case "${opt}" in
+  h|\?) show_usage
+      exit 0
+      ;;
+  s)  stage_dir=${OPTARG}
+      OC_STAGE_DIR=$(cd $stage_dir; pwd)
+      ;;
+  o)  output_dir=${OPTARG}
+      DOC_OUTPUT=$(cd $output_dir; pwd)
+      ;;
+  p)  plugin_dir=${OPTARG}
+      OC_PYANG_PLUGINS=$(cd $plugin_dir; pwd)
+      ;;
+  *)  show_usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+check_args
 
 $OC_STAGE_DIR/oc-stage.sh -r $OC_STAGE_DIR -p $OC_PYANG_PLUGINS -o $DOC_OUTPUT -b $PUSH_BRANCH
