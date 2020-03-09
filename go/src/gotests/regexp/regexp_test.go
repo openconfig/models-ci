8147b3ade7cca8a756151c81d2a946f5a1402ac2
wenovus <wenbli@google.com>
2020-03-09 16:22:32 -0700

    Change back regexp_test models argument from env var to flag

--- go/src/gotests/regexp/regexp_test.go
+++ go/src/gotests/regexp/regexp_test.go
@@ -1,8 +1,8 @@
 package regexp_test
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"regexp"
 	"testing"
 
@@ -240,5 +240,6 @@ func checkPattern(testData string, patterns []string) (compileErr error, matched
 // init sets up the test, particularly parsing the OpenConfig path which is
 // supplied as a command line argument.
 func init() {
-	ocdir = os.Getenv("OCDIR")
+	flag.StringVar(&ocdir, "ocdir", "../..", "Path to OpenConfig models repo")
+	flag.Parse()
 }
