2f6975a36e39a40e78e8d2207e1fdee06c2e77c9
wenovus <wenbli@google.com>
2020-04-07 16:39:55 -0700

    fix attempt

--- pyang-all/test.sh
+++ pyang-all/test.sh
@@ -48,7 +48,8 @@ run-pyang-head() {
   cd $REPODIR
   echo "THIS IS PYTHONPATH: $PYTHONPATH" # debug
   source ./env.sh
-  pip3 install --no-cache-dir -r $REPODIR/requirements.txt
+  ls -larth /workspace
+  pip3 install --no-cache-dir -r /workspace/$REPODIR/requirements.txt
   (bash $RESULTSDIR/script.sh pyang > $RESULTSDIR/$OUTFILE_NAME 2> $RESULTSDIR/$FAILFILE_NAME;
   go run /go/src/github.com/openconfig/models-ci/post_results/main.go -validator=pyang -version="-head" -modelRoot=$_MODEL_ROOT -repo-slug=$_REPO_SLUG -pr-branch=$_HEAD_BRANCH -commit-sha=$COMMIT_SHA)
 }
