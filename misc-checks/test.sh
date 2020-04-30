5a087d1a910d30c8021a124f18f65f35816cad6e
wenovus <wenbli@google.com>
2020-04-30 12:52:13 -0700

    fix attempt

--- misc-checks/test.sh
+++ misc-checks/test.sh
@@ -33,9 +33,8 @@ cat $RESULTSDIR/*.pr-file-parse-log > $RESULTSDIR/pr-file-parse-log 2>> $FAILFIL
 # changed-files.txt
 REPODIR=$RESULTSDIR/base_repo
 git clone -b $_HEAD_BRANCH "git@github.com:$_REPO_SLUG.git" $REPODIR
-find $REPODIR
 cd $REPODIR
-BASE_COMMIT=$(git merge-base $COMMIT_SHA master)
+BASE_COMMIT=$(git merge-base $COMMIT_SHA origin/master)
 git diff --name-only $BASE_COMMIT | grep -E '.*\.yang$' > $RESULTSDIR/changed-files.txt 2>> $FAILFILE
 
 # master-file-parse-log
