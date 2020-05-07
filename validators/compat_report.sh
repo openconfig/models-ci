60d4c19ec83a7c452b59c49c8b15a833cdb2907d
wenovus <wenbli@google.com>
2020-05-06 19:37:16 -0700

    Add pr-number to post_results call for compat-report

--- validators/compat_report.sh
+++ validators/compat_report.sh
@@ -10,4 +10,4 @@ if ! [ -s $USERCONFIG_DIR/compat-report-validators.txt ]; then
   exit 0
 fi
 
-$GOPATH/bin/post_results -validator=compat-report -modelRoot=$_MODEL_ROOT -repo-slug=$_REPO_SLUG -pr-branch=$_HEAD_BRANCH -commit-sha=$COMMIT_SHA
+$GOPATH/bin/post_results -validator=compat-report -modelRoot=$_MODEL_ROOT -repo-slug=$_REPO_SLUG -pr-branch=$_HEAD_BRANCH -commit-sha=$COMMIT_SHA -pr-number=$_PR_NUMBER
