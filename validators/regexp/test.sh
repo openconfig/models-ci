a6f30f8d5f4ffbfd29f013e461a01ef90c397458
wenovus <wenbli@google.com>
2020-12-23 12:43:29 -0800

    Update regexp to use the new test repo

--- validators/regexp/test.sh
+++ validators/regexp/test.sh
@@ -9,10 +9,27 @@ if ! stat $RESULTSDIR; then
   exit 0
 fi
 
-if go test -v gotests/regexp > $OUTFILE 2> $FAILFILE; then
+XSDFAILFILE=$RESULTSDIR/xsdfail
+pip3 install pyang
+if $OCDIR=$_MODEL_ROOT $GOPATH/src/github.com/openconfig/pattern-regex-tests/pytests/pattern_test.sh > $OUTFILE 2> $XSDFAILFILE; then
+  # Delete fail file if it's empty and the script passed.
+  find $XSDFAILFILE -size 0 -delete
+else
+  echo "## RFC7950 pattern statement" >> $FAILFILE
+  cat $FAILFILE $XSDFAILFILE > $FAILFILE
+  echo "" >> $FAILFILE
+fi
+
+POSIXFAILFILE=$RESULTSDIR/xsdfail
+if $GOPATH/bin/gotests -model-root=$_MODEL_ROOT $GOPATH/src/github.com/openconfig/pattern-regex-tests/testdata/regexp-test.yang >> $OUTFILE 2> $POSIXFAILFILE; then
   # Delete fail file if it's empty and the script passed.
   find $FAILFILE -size 0 -delete
+else
+  echo "## posix-pattern statement" >> $FAILFILE
+  cat $FAILFILE $POSIXFAILFILE > $FAILFILE
+  echo "" >> $FAILFILE
 fi
+
 $GOPATH/bin/post_results -validator=regexp -modelRoot=$_MODEL_ROOT -repo-slug=$_REPO_SLUG -pr-number=$_PR_NUMBER -commit-sha=$COMMIT_SHA -branch=$BRANCH_NAME
 BADGEFILE=$RESULTSDIR/upload-badge.sh
 if stat $BADGEFILE; then
