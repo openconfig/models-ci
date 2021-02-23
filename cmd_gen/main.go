9d9f9151ab6c6fbdd9bfae25cbb7c96607beef3e
wenovus <wenbli@google.com>
2021-02-23 12:24:23 -0800

    Run go get

--- cmd_gen/main.go
+++ cmd_gen/main.go
@@ -229,7 +229,7 @@ function run-dir() {
   echo $cmd "${options[@]}" "$@" > ${prefix}cmd
   status=0
   $($cmd "${options[@]}" "${script_options[@]}" "$@" &> ${prefix}pass) || status=1
-  $(cd "$outdir" && go build &> ${prefix}pass) || status=1
+  $(cd "$outdir" && go get ./... && go build &> ${prefix}pass) || status=1
   if [[ $status -eq "1" ]]; then
     mv ${prefix}pass ${prefix}fail
   fi
