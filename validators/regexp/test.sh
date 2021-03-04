cb322265bbd20cf99fdf17471e89817407b62435
wenovus <wenbli@google.com>
2021-03-03 17:42:47 -0800

    Fix bash bugs

--- validators/regexp/test.sh
+++ validators/regexp/test.sh
@@ -26,11 +26,11 @@ setup
 ########################## regexp #############################
 FAIL=0
 
-TESTFILES=$(find "$ROOT_DIR/regexp-tests" -name "*.yang" -print0)
-
+TESTFILES_FILE="$(mktemp)"
+find "$ROOT_DIR/regexp-tests" -name "*.yang" -print0 > "$TESTFILES_FILE"
 echo '## RFC7950 `pattern` statement' >> $FAILFILE
 XSDFAILFILE=$RESULTSDIR/xsdfail
-if echo "$TESTFILES" | OCDIR=$_MODEL_ROOT xargs -0 $GOPATH/src/github.com/openconfig/pattern-regex-tests/pytests/pattern_test.sh > $OUTFILE 2> $XSDFAILFILE; then
+if cat "$TESTFILES_FILE" | OCDIR=$_MODEL_ROOT xargs -0 $GOPATH/src/github.com/openconfig/pattern-regex-tests/pytests/pattern_test.sh > $OUTFILE 2> $XSDFAILFILE; then
   echo "Passed." >> $FAILFILE
 else
   FAIL=1
@@ -42,7 +42,7 @@ echo "" >> $FAILFILE
 
 echo '## `posix-pattern` statement' >> $FAILFILE
 POSIXFAILFILE=$RESULTSDIR/xsdfail
-if echo "$TESTFILES" | xargs -0 $GOPATH/bin/gotests -model-root=$_MODEL_ROOT >> $OUTFILE 2> $POSIXFAILFILE; then
+if cat "$TESTFILES_FILE" | xargs -0 $GOPATH/bin/gotests -model-root=$_MODEL_ROOT >> $OUTFILE 2> $POSIXFAILFILE; then
   echo "Passed." >> $FAILFILE
 else
   FAIL=1
